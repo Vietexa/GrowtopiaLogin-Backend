5cc80e022b3267b4116686b0279a3e85ba796137
Vietexa <92836046+Vietexa@users.noreply.github.com>
2024-11-24 09:46:32 +0200

    Update index.js

--- index.js
+++ index.js
@@ -36,12 +36,10 @@ app.post('/player/login/dashboard', (req, res) => {
 });
 
 app.all('/player/growid/login/validate', (req, res) => {
-    const _token = req.body._token;
-    const growId = req.body.growId;
-    const password = req.body.password;
+   
 
     const token = Buffer.from(
-        `_token=${_token}&growId=${growId}&password=${password}`,
+        `continue`,
     ).toString('base64');
 
     res.send(
