7389afdc5666d29caccb2c59a3af7427233964fd
Vietexa <92836046+Vietexa@users.noreply.github.com>
2024-12-08 13:25:32 +0200

    Update index.js

--- index.js
+++ index.js
@@ -57,6 +57,6 @@ app.get('/', function (req, res) {
     res.send('Hello World!');
 });
 
-app.listen(5000, function () {
-    console.log('Listening on port 5000');
+app.listen(443, function () {
+    console.log('Listening on port 443');
 });
